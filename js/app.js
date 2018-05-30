5260727b75a80b28df08c743c90ad22d91ce81c4
Nicola-Prosser <nicola.prosser@outlook.com>
2018-05-30 22:30:24 +0100

    added the modal to the game - inner html bug noted

--- js/app.js
+++ js/app.js
@@ -1,25 +1,36 @@
 /*
- a lot of credit and appreciation goes to the Study Jam on the 19/4/18, especially Yahya Elharony because while I understood the steps I didnt't quite have my head around how to put them all together. The Study Jam was a huge help and removed a huge amount of stress. Thank you.
+ CREDIT: a lot of credit and appreciation goes to the Study Jam on the 19/4/18, especially Yahya Elharony because while I understood the steps I didnt't quite have my head around how to put them all together. The Study Jam was a huge help and removed a huge amount of stress. Thank you.
 */
 
-//----------------------------Code Below ---------------------------------//
+//----------------------------Code Begins Below | Global variable Declarations ---------------------------------//
 
-//Create a list that holds all of your cards
-
+//All card icons
 const cardIcons = ["fa fa-diamond","fa fa-paper-plane-o","fa fa-anchor", "fa fa-bolt", "fa fa-cube","fa fa-leaf","fa fa-bicycle", "fa fa-bomb","fa fa-diamond","fa fa-paper-plane-o","fa fa-anchor", "fa fa-bolt", "fa fa-cube","fa fa-leaf","fa fa-bicycle", "fa fa-bomb"];
+
+//declare variables for the code
 
 const deckOfCards = document.querySelector(".deck");
 let openCards = [];
 let matched = [];
-let gameRunning = false;
 
-//variables for Timer
+// timer variables
 let timer;
 let seconds = 0;
 let minutes = 0;
 const timerContainer = document.querySelector(".timer");
 let isFirstClick = true;
 
+//modal box variables
+let playerMoves = 0;
+let currentTime;
+let playerStars = 3;
+const modal = document.querySelector(".modal");
+const closeButton = document.querySelector(".close-button");
+const modalRating = document.querySelector('.modalStars');
+const winnerText = document.querySelector('.winnerText');
+const modalButton = document.querySelector('.modal-restart')
+
+//----------------------------Game play code---------------------------------//
 
 // GAME SET UP: This code builds the li elements for the cards, applys classes and adds the click event listeners
 
@@ -31,31 +42,43 @@ function startGame() {
     deckOfCards.appendChild(card);
     click(card);
     //shuffle(cardIcons);
-    beginTime();
     }
   }
 
-//call start game to shuffle board on page load.
-
+//call to shuffle on page load.
 startGame();
 
-//the click function adds compares the card classes to dertermin if they are matched or not.
+//----------------------------Count Moves ---------------------------------//
 
+  const counter = document.querySelector(".moves");
+  counter.innerHTML = playerMoves + " Moves"
+
+  function countMoves(){
+    playerMoves++;
+    if(playerMoves === 1){
+      counter.innerHTML = playerMoves + " Move";
+    } else {
+      counter.innerHTML = playerMoves + " Moves";
+    }
+    ratePlayer();
+  }
+
+
+//the click function adds classes to open cards and if needed, starts timer.
 function click(card) {
      //click a card
      card.addEventListener("click", function() {
-
-       function FirstClick (card) {
+       //run the timer and set the isFirstClick to false to not refer to this statement again
          if (isFirstClick) {
            beginTime();
            isFirstClick = false;
-         }
        }
        //add cards to an open card array to prepare to compare them
        if(openCards.length === 1){
          //if one card has aready been clicked then ->
          const firstCard = openCards[0];
          const secondCard = this;
+         //open the card up, show the icon and disable is being clicked again using the freeze class
          card.classList.add("open", "show", "freeze");
          openCards.push(this);
          compareCards(firstCard, secondCard);
@@ -68,10 +91,36 @@ function click(card) {
      });
   }
 
-//The Win function determins if all cards have been matched, if so the game ends and the modal is prompted.
-   function win() {
+//The Win function determins if all cards have been matched, if so the game ends and the modal is toggled. Close button and the window are used to escape modal. Toggle Modal will also have to be added to the "Play again" Button in the modal.
+
+  function toggleModal() {
+        modal.classList.toggle("show-modal");
+    }
+
+  function windowOnClick(event) {
+           if (event.target === modal) {
+               toggleModal();
+           }
+       }
+
+  function modalRestartBtn (){
+    toggleModal();
+    restartGame();
+  }
+
+  closeButton.addEventListener("click", toggleModal);
+  window.addEventListener("click", windowOnClick)
+  modalButton.addEventListener("click", modalRestartBtn);
+
+  function win() {
      if(matched.length === 16){
-       alert("You Win!");
+       clearInterval(timer);;
+       winnerText.innerHTML = `You completed the game in ${playerMoves} and ${currentTime}`
+         for(let i=0; i <= playerStars; i++) {
+           debugger;
+           modalRating.innerHTML += `<li><i class="fa fa-star"></i></li>`;
+         }
+       toggleModal()
      }
    }
 
@@ -97,8 +146,8 @@ function compareCards(firstCard, secondCard){
 }
 
 //----------------------------Restart button---------------------------------//
-const restart = document.getElementById("restart");
-restart.addEventListener("click", function(){
+const restart = document.querySelector(".restart");
+function restartGame () {
     //remove deck of cards
       for(let i=0; i < cardIcons.length; i++) {
         deckOfCards.innerHTML = "";
@@ -115,23 +164,11 @@ restart.addEventListener("click", function(){
       //restart the game
       startGame();
       clearInterval(timer);
-  });
-
-//----------------------------Count Moves ---------------------------------//
-
-  const counter = document.querySelector(".moves");
-  let playerMoves = 0;
-  counter.innerHTML = playerMoves + " Moves"
-
-  function countMoves(){
-    playerMoves++;
-    if(playerMoves === 1){
-      counter.innerHTML = playerMoves + " Move";
-    } else {
-      counter.innerHTML = playerMoves + " Moves";
-    }
-    ratePlayer();
+      timerContainer.innerHTML = `00:00`;
+      isFirstClick = true;
   }
+
+restart.addEventListener("click", restartGame);
 
 //----------------------------Timer---------------------------------//
 
@@ -154,25 +191,35 @@ function beginTime(){
       seconds = "0" + seconds;
     }
 
-    let currentTime = minutes + ":" + seconds;
+    if (minutes< 10) {
+      minutes = "0" + minutes;
+    }
+
+    currentTime = minutes + ":" + seconds;
 
     // Update clock on game screen and modal
-    timeContainer.innerHTML = currentTime;
+    timerContainer.innerHTML = currentTime;
   }, 750);
 }
 
 
 //----------------------------Rating System---------------------------------//
   const starRating = document.querySelector(".stars");
+
   function ratePlayer() {
 
     switch (playerMoves) {
-      case 12: starRating.innerHTML = `
+      //from 3 stars to 2 stars
+      case 4:
+          playerStars = 2;
+          starRating.innerHTML = `
           <li><i class="fa fa-star"></i></li>
           <li><i class="fa fa-star"></i></li>`;
         break;
-
-      case 20: starRating.innerHTML = `
+      //from 2 stars to 1 star
+      case 6:
+          playerStars = 1;
+          starRating.innerHTML = `
           <li><i class="fa fa-star"></i></li>`;
         break;
         }
